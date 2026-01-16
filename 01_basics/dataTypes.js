99014ff70d7c3ee318569c191dee9e3f2d3fd7d1
callmeUj <167564643+ujjwalsingh99313@users.noreply.github.com>
2026-01-16 05:59:51 +0000

    new changes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,13 @@
+"use strict" ; // treat all the JS code as modern JS code
+
+// alert(4+3) //we are using nodejs  not browser
+
+let name="Ujjwal"
+let age =24
+let isLogin=true
+
+
+///number => 2 to power 53
+//bigint => more than 2 to power 53
+
+console.log(typeof age);
